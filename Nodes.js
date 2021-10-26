cecc4deb7b706296e0ebb8c3178c652db4dbb22c
Matthieu Melin <matthieumelin62@gmail.com>
2021-10-27 00:25:02 +0200

    Nodes functions added getAllNodes added getNodeByName

--- Nodes.js
+++ Nodes.js
@@ -8,51 +8,74 @@ class Nodes {
         this.panel = options['panel'];
 
         if (!options['api_key']) return new Error("You must put an application key.");
-        this.interval = options["interval"] || 1000 * 3;
-
         this.apiKey = options["api_key"];
-
+        this.interval = options["interval"] || 1000 * 3;
         this.debug = options['debug'] || false;
 
+        this.running = false;
         this.connected = false;
-        
+
         this.nodes = [];
-        
+
         this.prefix = "[Nodes]";
 
-        // Auto post
-        this.autoPost();
+        // Start
+        this.start();
     }
 
-    // Auto post request
-    async autoPost() {
-        setInterval(async () => await this.post(), this.interval);
+    // Start
+    async start() {
+        if (this.running) return new Error(`${this.prefix} Already started.`);
+        // Running
+        this.running = true;
+        // Auto fetch
+        await this.#autoFetchNodes();
     }
 
-    // Post request
-    async post() {
+    // Stop
+    async stop() {
+        if (!this.running) return new Error(`${this.prefix} Not started.`);
+        this.running = false;
+    }
+
+    // Auto fetch nodes
+    async #autoFetchNodes() {
+        if (!this.running) return new Error(`${this.prefix} Not started.`);
+        setInterval(async () => await this.#asyncFetchNodes(), this.interval);
+    }
+
+    status(res) {
+        if (res.status === 200) {
+            if (!this.connected) {
+                this.connected = true;
+                if (this.debug) console.log(`${this.prefix} Nodes successfully connected.`);
+            }
+            const datas = res.data.data;
+
+            datas.forEach(data => this.nodes.push(data));
+
+            if (this.debug) console.log(`${this.prefix} Successfully fetched nodes.`);
+        }
+    }
+
+    async #asyncFetchNodes() {
         await axios(`${this.panel}/api/application/nodes`, {
+            method: "GET",
             headers: {
                 Authorization: `Bearer ${this.apiKey}`
             }
         }).then(res => {
-            // Request accepted
-            if (res.status === 200) {
-                if (!this.connected) {
-                    this.connected = true;
-                    if (debug) console.log(`${this.prefix} Nodes successfully connected.`);
-                }
-                const datas = res.data.data;
-
-                datas.forEach(data => this.nodes.push(data));
-            
-                if (debug) console.log(`${this.prefix} Successfully fetched nodes`);
-            }
+            this.status(res);
         });
     }
+
     // Get all nodes
     getAllNodes() {
         return this.nodes;
+    }
+    // Get node by name
+    getNodeByName(name) {
+        return this.nodes.find(node => node.attributes.name.replace(" ", "").toLowerCase() === name.replace(" ", "").toLowerCase());
     }
 }
 
